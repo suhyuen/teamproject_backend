ee7309163006a61a9e022e8a510465da836e08e3
kimjaewoo0049 <kim1656@naver.com>
2024-03-22 16:17:42 +0900

    dogposts mergi

--- src/main/java/com/animalCommunity/project/dtos/TestDto.java
+++ src/main/java/com/animalCommunity/project/dtos/TestDto.java
@@ -1,6 +1,9 @@
 package com.animalCommunity.project.dtos;
 
+import com.animalCommunity.project.models.User;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -9,4 +12,13 @@ import lombok.*;
 @AllArgsConstructor
 public class TestDto {
     private String testString;
+    private int uid;
+    private int userUid;
+    private String title;
+    private String content;
+    private int pageUid;
+    private int mainUid;
+    private LocalDateTime createdAt;
+    private int likeCount;
+    private int commentCount;
 }
