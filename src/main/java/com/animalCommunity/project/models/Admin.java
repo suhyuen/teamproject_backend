c0c52fb2c656b8a857a1df8e563904b48f2a649d
Song Jungeun <jungeun2474@gmail.com>
2024-04-02 10:40:04 +0900

    admin_function

--- src/main/java/com/animalCommunity/project/models/Admin.java
+++ src/main/java/com/animalCommunity/project/models/Admin.java
@@ -1,19 +1,26 @@
 package com.animalCommunity.project.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Admin {
     private int uid;
     private int userUid;
+    private String nickname;
     private int pageUid;
+    private int mainUid;
     private String title;
     private String content;
     private int viewer;
     private Date createdAt;
     private String deleteyn;
+
 }
